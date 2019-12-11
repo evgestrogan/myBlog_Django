9c2401bb3cf68e50b5570f4655941e8f0ab26422
Evgeniy Stroganov <evgestrogan@gmail.com>
2019-12-11 11:05:48 +0300

    added handler class

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from blog import views
 app_name = 'blog'
 # пространство имен приложения
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          # извлечение значений из URL'a. <parametr> возвращается в виде строки,
          # потому мы используем конвертер
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,23 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from blog.models import Post
 
 
-def post_list(request):  # main page
+class PostListView(ListView):  # аналог функции post_list()
+    queryset = Post.published.all()
+    # это свой менеджер модели который вернет список постов в обратном порядке публикации
+    # queryset - когда свой менеджер, model = Post.objects.all() - чтобы использовать стандартный
+    context_object_name = 'posts'
+    # переменная контекста
+    paginate_by = 3
+    # постраничное отображение. По 3 объекта на странице
+    template_name = 'blog/post/list.html'
+    # использование указанного шаблона
+
+
+def post_list(request):  # main page (сейчас не используется. Был заменен классом-обработчиком)
     object_list = Post.published.all()
     # это свой менеджер модели который вернет список постов в обратном порядке публикации
     paginator = Paginator(object_list, 3)
@@ -26,4 +39,3 @@ def post_detail(request, year, month, day, post):  # page with post
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
-
